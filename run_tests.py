bd799146280a35f3d7a1ea8e2abb9d8abec09d43
Ben Puderer <bpuderer@yahoo.com>
2016-08-21 15:27:01 -0400

    collect-only option

--- run_tests.py
+++ run_tests.py
@@ -1,4 +1,4 @@
-"""Wrapper for nose/nose2."""
+"""Wrapper for nose/nose2"""
 
 import argparse
 import glob
@@ -9,22 +9,25 @@ import subprocess
 
 
 def main():
-    parser = argparse.ArgumentParser(description='nose/nose2 wrapper script')
-    parser.add_argument('--testenv', '-testenv', default='DEFAULT',
+    parser = argparse.ArgumentParser(description='nose/nose2 wrapper script',
+                                     formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument('--testenv', '-te', default='DEFAULT',
                         help='case sensitive section in test_settings.cfg')
-    parser.add_argument('--tests', '-tests', nargs='+',
+    parser.add_argument('--tests', '-t', nargs='+',
                         help='Ex. tests/test_example.py tests.test_example \
                         tests.test_example:ExampleTestCase.test_str_ends_in_r')
-    parser.add_argument('--attrib', '-a', nargs='+', help='Args are logically \
+    parser.add_argument('--attr', '-a', nargs='+', help='Args are logically \
                         ORed. Arg with comma delimeters is ANDed. \
                         Ex. slow tags=tag2 Ex. slow,tags=tag2')
-    parser.add_argument('--eval_attrib', '-A', nargs='+', help='python expression for attributes')
-    parser.add_argument('--quiet', '-quiet', action='store_true', default=False)
-    parser.add_argument('--nose2', '-nose2', action='store_true',
+    parser.add_argument('--eval_attr', '-A', nargs='+', help='python expression for attributes')
+    parser.add_argument('--quiet', '-q', action='store_true', default=False)
+    parser.add_argument('--nose2', '-n2', action='store_true',
                         default=False, help='use nose2 instead of nose')
-    parser.add_argument('--xml_out', '-xml_out', action='store_true',
+    parser.add_argument('--collect_only', '-c', action='store_true',
+                        default=False, help='collect and output test names, don\'t run tests')
+    parser.add_argument('--xml', action='store_true',
                         default=False, help='write test results xunit xml')
-    parser.add_argument('--html_out', '-html_out', action='store_true',
+    parser.add_argument('--html', action='store_true',
                         default=False, help='write test results in HTML using ant JUnitReport')
     args = parser.parse_args()
 
@@ -40,21 +43,29 @@ def main():
         cmd = 'python -m nose2 --config framework/nose2.cfg'
     else:
         cmd = 'python -m nose --nocapture --nologcapture'
+
     if not args.quiet:
         cmd += ' -v'
+
+    if args.collect_only:
+        cmd += ' --collect-only'
+
     if args.tests:
         cmd += ' ' + ' '.join(args.tests)
-    if args.attrib:
+
+    if args.attr:
         if args.nose2:
-            cmd += ' -A ' + ' -A '.join(args.attrib)
+            cmd += ' -A ' + ' -A '.join(args.attr)
         else:
-            cmd += ' -a ' + ' -a '.join(args.attrib)
-    if args.eval_attrib:
+            cmd += ' -a ' + ' -a '.join(args.attr)
+
+    if args.eval_attr:
         if args.nose2:
-            cmd += ' -E ' + ' -E '.join(args.eval_attrib)
+            cmd += ' -E ' + ' -E '.join(args.eval_attr)
         else:
-            cmd += ' -A ' + ' -A '.join(args.eval_attrib)
-    if args.xml_out or args.html_out:
+            cmd += ' -A ' + ' -A '.join(args.eval_attr)
+
+    if args.xml or args.html:
         if args.nose2:
             cmd += ' --junit-xml'
         else:
@@ -66,7 +77,7 @@ def main():
         # if any test fails a non-zero exit status is returned
         pass
 
-    if args.html_out:
+    if args.html:
         if args.nose2:
             cmd = 'python -m framework.split_xunitxml --infile reports/nose2-junit.xml'
         else:
