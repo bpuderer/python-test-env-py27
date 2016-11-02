a31768177125b3aaffe7b3b75a4e7217e4fc87bb
Ben Puderer <bpuderer@yahoo.com>
2016-11-01 21:59:41 -0400

    selenium example

---
+++ tests/test_selenium_example.py
@@ -0,0 +1,25 @@
+import unittest
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+
+class SeleniumExample(unittest.TestCase):
+
+    def setUp(self):
+        self.driver = webdriver.Firefox()
+
+    def tearDown(self):
+        self.driver.quit()
+
+    def test_selenium_example(self):
+        driver = self.driver
+        driver.get("https://en.wikipedia.org/wiki/Monty_Python%27s_Flying_Circus")
+        WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.LINK_TEXT, "John Cleese"))).click()
+        WebDriverWait(driver, 5).until(EC.title_is("John Cleese - Wikipedia"))
+
+        driver.get_screenshot_as_file('screenshots/jmc.png')
+
+        ps = driver.page_source
+        self.assertIn("John Marwood Cleese", ps)
